c3dd0ee0922834e04068b30e02a26e685e166f14
Gaurav Juvekar <gauravjuvekar@gmail.com>
2022-07-30 12:17:58 -0700

    doc: Add autodoc import paths

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/'))
+
 project = 'remote-email-filtering'
 copyright = '2022, Gaurav Juvekar'
 author = 'Gaurav Juvekar'
