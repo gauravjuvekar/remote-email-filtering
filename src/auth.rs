37c7f7adddaa4239186a702652aea8ca3a2e3b48
Gaurav Juvekar <gauravjuvekar@gmail.com>
2026-01-11 22:49:31 +0530

    merge google-oauth2-auth into main binary

---
+++ src/auth.rs
@@ -0,0 +1,359 @@
+use oauth2::TokenResponse;
+use secrecy::SecretString;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use tracing::debug;
+
+#[derive(Error, Debug)]
+pub enum AuthorizationError {
+    #[error("invalid config while parsing '{field}'")]
+    ConfigError {
+        source: oauth2::url::ParseError,
+        field: String,
+    },
+
+    #[error("unexpected response: {msg}")]
+    ResponseError {
+        source: Option<anyhow::Error>,
+        msg: String,
+    },
+
+    #[error("timed out waiting for user authorization to complete")]
+    Timeout,
+
+    #[error("unknown auth failure")]
+    Unknown { source: anyhow::Error },
+}
+
+#[derive(Clone, Debug, PartialEq, clap::ValueEnum)]
+pub enum Provider {
+    Google,
+    Microsoft,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GoogleProviderConfig {
+    client_id: String,
+    client_secret: SecretString,
+    auth_url: String,
+    token_url: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MicrosoftProviderConfig {
+    client_id: String,
+    devicecode_url: String,
+    auth_url: String,
+    token_url: String,
+}
+
+pub enum ProviderConfig {
+    Google(GoogleProviderConfig),
+    Microsoft(MicrosoftProviderConfig),
+}
+
+fn config_to_provider(config: &ProviderConfig) -> Provider {
+    match config {
+        ProviderConfig::Google(_) => Provider::Google,
+        ProviderConfig::Microsoft(_) => Provider::Microsoft,
+    }
+}
+
+pub fn serialize_secret_string<S: serde::Serializer>(
+    secret: &SecretString,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    use secrecy::ExposeSecret;
+    serializer.serialize_str(secret.expose_secret())
+}
+
+pub fn serialize_optional_secret_string<S: serde::Serializer>(
+    secret: &Option<SecretString>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    match secret {
+        Some(s) => serialize_secret_string(s, serializer),
+        None => Option::<String>::serialize(&None, serializer),
+    }
+}
+
+/// Secrets enought to obtain access tokens from refresh tokens
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PersistedSecrets {
+    client_id: String,
+    #[serde(serialize_with = "serialize_optional_secret_string")]
+    client_secret: Option<SecretString>,
+    #[serde(serialize_with = "serialize_secret_string")]
+    refresh_token: SecretString,
+    #[serde(serialize_with = "serialize_secret_string")]
+    access_token: SecretString,
+    access_token_validity: chrono::DateTime<chrono::Utc>,
+    token_url: String,
+}
+
+impl secrecy::SerializableSecret for PersistedSecrets {}
+
+fn get_request_fn(
+) -> impl Fn(http::Request<Vec<u8>>) -> std::result::Result<oauth2::HttpResponse, reqwest::Error> {
+    fn translate_request(
+        req: oauth2::HttpRequest,
+        client: &reqwest::blocking::Client,
+    ) -> reqwest::blocking::Request {
+        let (parts, body) = req.into_parts();
+        let uri_str: std::string::String = parts.uri.to_string();
+        client
+            .request(parts.method, uri_str)
+            .headers(parts.headers)
+            .body(body)
+            .build()
+            .expect("reqwest client should build")
+    }
+
+    fn translate_response(res: reqwest::blocking::Response) -> oauth2::HttpResponse {
+        let mut builder = http::response::Builder::new();
+        builder = builder.status(res.status()).version(res.version());
+        {
+            let headers = builder.headers_mut().expect("valid http response");
+            for (key, value) in res.headers().iter() {
+                headers.append(key.clone(), value.clone());
+            }
+        }
+        let u8_body: Vec<u8> = res.bytes().unwrap().into();
+        builder.body(u8_body).unwrap()
+    }
+
+    let req_client = reqwest::blocking::ClientBuilder::new()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .expect("reqwest client should build");
+
+    let http_client_fn = move |req: oauth2::HttpRequest| {
+        let resp = req_client.execute(translate_request(req, &req_client))?;
+        Result::<http::Response<Vec<u8>>, reqwest::Error>::Ok(translate_response(resp))
+    };
+
+    http_client_fn
+}
+
+pub fn authorize(config: &ProviderConfig) -> Result<PersistedSecrets, AuthorizationError> {
+    let auth_url_str;
+    let token_url_str;
+    match config {
+        ProviderConfig::Google(c) => {
+            auth_url_str = c.auth_url.clone();
+            token_url_str = c.token_url.clone();
+        }
+        ProviderConfig::Microsoft(c) => {
+            auth_url_str = c.auth_url.clone();
+            token_url_str = c.token_url.clone();
+        }
+    };
+
+    let auth_url =
+        oauth2::AuthUrl::new(auth_url_str).map_err(|e| AuthorizationError::ConfigError {
+            source: e,
+            field: "auth_url".to_string(),
+        })?;
+    let token_url = oauth2::TokenUrl::new(token_url_str.clone()).map_err(|e| {
+        AuthorizationError::ConfigError {
+            source: e,
+            field: "token_url".to_string(),
+        }
+    })?;
+
+    struct RemainingTimeSecs(std::sync::atomic::AtomicU64);
+    impl std::fmt::Display for RemainingTimeSecs {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "Waiting for {} seconds",
+                self.0.load(std::sync::atomic::Ordering::Relaxed)
+            )
+        }
+    }
+
+    match config {
+        ProviderConfig::Google(c) => {
+            // Google uses RFC6749-section 4.1 Authorization Code Grant with RFC8252-section 7.3
+            // Lopback Interface Redirection (with PKCE)
+            use std::net::{SocketAddr, TcpListener};
+
+            // Try both IPv4 and IPv6 loopback address
+            let bind_addrs = [
+                SocketAddr::from(([127, 0, 0, 1], 0)),
+                SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 1], 0)),
+            ];
+
+            let listener = TcpListener::bind(&bind_addrs[..]).expect("should have free ports");
+            let bind_addr = listener
+                .local_addr()
+                .expect("should have started listening");
+            listener
+                .set_nonblocking(true)
+                .expect("listener should be non-blocking");
+            let redirect_url_str = format!("http://{}", bind_addr);
+            debug!("Listening on {}", redirect_url_str);
+
+            let redirect_url = oauth2::RedirectUrl::new(redirect_url_str.clone())
+                .expect("valid redirect url we are already listening on");
+
+            debug!("Setting redirect URL to {}", redirect_url);
+
+            let (pkce_code_challenge, pkce_code_verifier) =
+                oauth2::PkceCodeChallenge::new_random_sha256();
+
+            let client = {
+                use secrecy::ExposeSecret;
+                oauth2::basic::BasicClient::new(oauth2::ClientId::new(c.client_id.clone()))
+                    .set_client_secret(oauth2::ClientSecret::new(
+                        c.client_secret.expose_secret().to_string(),
+                    ))
+            }
+            .set_auth_uri(auth_url)
+            .set_token_uri(token_url)
+            .set_redirect_uri(redirect_url);
+
+            let (display_url, csrf_state) = client
+                .authorize_url(oauth2::CsrfToken::new_random)
+                .add_scope(oauth2::Scope::new("https://mail.google.com".to_string()))
+                .set_pkce_challenge(pkce_code_challenge)
+                .url();
+
+            println!("Visit this URL on this device to authorize:\n\n\t{display_url}\n");
+
+            let (code, state) = {
+                let start = std::time::Instant::now();
+                let deadline = start + std::time::Duration::from_secs(300);
+
+                let remaining_time = status_line::StatusLine::new(RemainingTimeSecs(
+                    std::sync::atomic::AtomicU64::new(
+                        (deadline - std::time::Instant::now()).as_secs(),
+                    ),
+                ));
+
+                let mut stream = None;
+                while std::time::Instant::now() < deadline {
+                    remaining_time.0.store(
+                        (deadline - std::time::Instant::now()).as_secs(),
+                        std::sync::atomic::Ordering::Relaxed,
+                    );
+
+                    match listener.accept() {
+                        Ok((s, _socket_addr)) => {
+                            stream = Some(s);
+                            break;
+                        }
+                        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                            std::thread::sleep(std::time::Duration::from_secs(1));
+                            continue;
+                        }
+                        Err(e) => return Err(AuthorizationError::Unknown { source: e.into() }),
+                    };
+                }
+
+                drop(remaining_time);
+
+                match stream {
+                    None => {
+                        return Err(AuthorizationError::Timeout);
+                    }
+                    Some(mut s) => {
+                        let mut reader = std::io::BufReader::new(&s);
+
+                        let mut line = String::new();
+                        std::io::BufRead::read_line(&mut reader, &mut line).map_err(|e| {
+                            AuthorizationError::ResponseError {
+                                source: Some(e.into()),
+                                msg: "expected a whole line".to_string(),
+                            }
+                        })?;
+
+                        let get_query = line.split_ascii_whitespace().nth(1).ok_or_else(|| {
+                            AuthorizationError::ResponseError {
+                                source: None,
+                                msg: "expected a 3 part GET line".to_string(),
+                            }
+                        })?;
+
+                        let get_url = url::Url::parse(&(redirect_url_str.clone() + &get_query))
+                            .expect("valid GET URL");
+
+                        let mut query_dict = std::collections::HashMap::<String, String>::from_iter(
+                            get_url.query_pairs().into_owned(),
+                        );
+
+                        let code = query_dict.remove("code").ok_or_else(|| {
+                            AuthorizationError::ResponseError {
+                                source: None,
+                                msg: "'code' GET parameter missing".to_string(),
+                            }
+                        })?;
+                        let state = query_dict.remove("state").ok_or_else(|| {
+                            AuthorizationError::ResponseError {
+                                source: None,
+                                msg: "'state' GET parameter missing".to_string(),
+                            }
+                        })?;
+
+                        let message = "You can close this page.";
+                        let response = format!(
+                            concat!(
+                                "HTTP/1.1 200 OK\r\n",
+                                "content-length: {}\r\n",
+                                "\r\n",
+                                "{}",
+                            ),
+                            message.len(),
+                            message
+                        );
+                        std::io::Write::write(&mut s, response.as_bytes()).expect(
+                            "hopefully the browser didn't timeout and close the connection",
+                        );
+
+                        (code, state)
+                    }
+                }
+            };
+            if state != *csrf_state.secret() {
+                return Err(AuthorizationError::ResponseError {
+                    source: None,
+                    msg: "CSRF token mismatch in response".to_string(),
+                });
+            }
+
+            // Exchange the code for standard oauth2 tokens
+
+            let http_client_fn = get_request_fn();
+
+            let tokens = client
+                .exchange_code(oauth2::AuthorizationCode::new(code))
+                .set_pkce_verifier(pkce_code_verifier)
+                .request(&http_client_fn)
+                .map_err(|e| AuthorizationError::Unknown { source: e.into() })?;
+
+            Ok(PersistedSecrets {
+                client_id: c.client_id.clone(),
+                client_secret: Some(c.client_secret.clone()),
+                refresh_token: tokens
+                    .refresh_token()
+                    .ok_or_else(|| AuthorizationError::ResponseError {
+                        source: None,
+                        msg: "missing refresh token in response".to_string(),
+                    })?
+                    .clone()
+                    .into_secret()
+                    .into(),
+                access_token: tokens.access_token().clone().into_secret().into(),
+                access_token_validity: chrono::Utc::now()
+                    + tokens
+                        .expires_in()
+                        .unwrap_or_else(|| std::time::Duration::from_secs(0)),
+                token_url: token_url_str,
+            })
+        }
+
+        ProviderConfig::Microsoft(c) => {
+            todo!()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod auth;
+
 use remote_email_filtering as ref_;
 
 use clap;
@@ -8,44 +10,69 @@ enum Commands {
     Filter,
 
     /// Login
-    Login,
+    Login(Login),
 }
 
-#[derive(Debug, clap::Args)]
-#[group(required = true, multiple = true)]
-struct Auth {
-    app_registration: std::path::PathBuf,
-    app_auth: std::path::PathBuf,
+#[derive(Debug, PartialEq, clap::Args)]
+struct Login {
+    provider: auth::Provider,
+
+    /// provider specific config file
+    config_json: std::path::PathBuf,
+
+    /// output file with authorized oauth2 tokens
+    authorized_json: std::path::PathBuf,
 }
 
 #[derive(Debug, clap::Parser)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
-
-    #[clap(flatten)]
-    auth: Auth,
 }
 
-fn main() {
+fn parse_config(
+    provider: auth::Provider,
+    config_path: std::path::PathBuf,
+) -> Result<auth::ProviderConfig, anyhow::Error> {
+    let string = std::fs::read_to_string(config_path)?;
+    match provider {
+        auth::Provider::Google => {
+            let config: auth::GoogleProviderConfig = serde_json::from_str(&string)?;
+            return Ok(auth::ProviderConfig::Google(config));
+        }
+        auth::Provider::Microsoft => {
+            let config: auth::MicrosoftProviderConfig = serde_json::from_str(&string)?;
+            return Ok(auth::ProviderConfig::Microsoft(config));
+        }
+    }
+}
+
+fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt()
-        .with_env_filter(
-            tracing_subscriber::filter::EnvFilter::from_default_env(),
-        )
+        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
 
     let args = <Cli as clap::Parser>::parse();
 
-    if args.command == Commands::Filter {
-        let my_filter =
-            ref_::actions::Action::Logic(Box::new(ref_::filters::DebugPrint));
+    match args.command {
+        Commands::Login(login) => {
+            let perstable_secret = auth::authorize(&parse_config(login.provider, login.config_json)?)?;
+            let file = std::fs::File::create(login.authorized_json)?;
+            let mut writer = std::io::BufWriter::new(file);
+            serde_json::to_writer(writer, &perstable_secret)?;
+            Ok(())
+        },
+        Commands::Filter => {
+            let my_filter = ref_::actions::Action::Logic(Box::new(ref_::filters::DebugPrint));
 
-        let spec = vec![(
-            ref_::types::Folder {
-                path: vec!["INBOX".to_string()],
-            },
-            vec![my_filter],
-        )];
-        ref_::filters::mainloop(&spec)
+            let spec = vec![(
+                ref_::types::Folder {
+                    path: vec!["INBOX".to_string()],
+                },
+                vec![my_filter],
+            )];
+            ref_::filters::mainloop(&spec);
+            Ok(())
+        }
     }
 }
